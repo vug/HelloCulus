a24d5b56cd9dfdc727e386d718bdb40035d6cef1
Uğur Güney <ugurguney@gmail.com>
2021-06-12 10:40:41 -0400

    Introduce GLUT to initialize OpenGL
    Looks like GLUT is deprecated. I can switch to a more modern library such as GLFW and GLAD later.

--- HelloCulus/src/main.cpp
+++ HelloCulus/src/main.cpp
@@ -1,5 +1,7 @@
-#include <iostream>
 #include <iomanip>
+#include <iostream>
+
+#include <glut.h>
 #include <Windows.h>
 
 #include <OVR_CAPI.h>
@@ -13,32 +15,6 @@ void printHmdInfo(const ovrHmdDesc& desc) {
 	std::cout << "  Resolution. Width: " << desc.Resolution.w << ", Height: " << desc.Resolution.h << std::endl;
 	std::cout << "  Display Refresh Rate: " << desc.DisplayRefreshRate << std::endl;
 	// AvailableHmdCaps, DefaultHmdCaps, AvailableTrackingCaps, DefaultTrackingCaps, DefaultEyeFov, MaxEyeFov
-}
-
-// Non-blocking keyboard input
-DWORD numEvents;
-INPUT_RECORD irInBuf[128]; // buffer to read into
-HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE); // input buffer handle
-bool keyboardInterrupt() {
-	bool shouldExit = false;
-	PeekConsoleInput(hStdin, irInBuf, 128, &numEvents);
-	if (numEvents > 0) {
-		ReadConsoleInput(hStdin, irInBuf, 128, &numEvents);
-		for (int i = 0; i < numEvents; i++) {
-			if (irInBuf[i].EventType != KEY_EVENT)
-				continue;
-			KEY_EVENT_RECORD ev = irInBuf[i].Event.KeyEvent;
-			if (!ev.bKeyDown)
-				continue;
-			switch (ev.uChar.AsciiChar) {
-			case 'q':
-			case 'Q':
-				shouldExit = true;
-			}
-			//std::cout << "key code: " << ev.wVirtualKeyCode << ", ...: " << ev.wVirtualScanCode << ", ...: " << ev.uChar.AsciiChar << std::endl;
-		}
-	}
-	return shouldExit;
 }
 
 void printPositionAndOrientation(ovrTrackingState ts, int timeStep) {
@@ -57,13 +33,50 @@ void printPositionAndOrientation(ovrTrackingState ts, int timeStep) {
 	}
 }
 
-int main() {
+int timeStep = 0;
+ovrSession session;
+void glutDisplay(void) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glBegin(GL_TRIANGLES);
+	glVertex3f(-0.5, -0.5, 0.0);
+	glVertex3f(0.5, 0.0, 0.0);
+	glVertex3f(0.0, 0.5, 0.0);
+	glEnd();
+
+	glutSwapBuffers();
+
+	ovrTrackingState ts = ovr_GetTrackingState(session, ovr_GetTimeInSeconds(), ovrTrue);
+	printPositionAndOrientation(ts, timeStep);
+	timeStep++;
+}
+
+void glutIdle() {
+	glutPostRedisplay();
+}
+
+void glutKeyboard(unsigned char key, int x, int y) {
+	if (key == 'q') {
+		glutDestroyWindow(glutGetWindow());
+
+		ovr_Destroy(session);
+		ovr_Shutdown();
+		std::cout << "Bye, Rift!" << std::endl;
+		exit(0);
+	}
+}
+
+int main(int argc, char** argv) {
 	std::cout << "Hello, Rift!" << std::endl;
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
+	auto win = glutCreateWindow("Points");
 
 	ovrResult result = ovr_Initialize(nullptr);
 	if (OVR_FAILURE(result)) { std::cout << "Initialization failed with result code " << result << std::endl;  return result; }
 
-	ovrSession session;
 	ovrGraphicsLuid luid;
 	result = ovr_Create(&session, &luid);
 	if (OVR_FAILURE(result)) { std::cout << "Creation failed with result code " << result << std::endl; ovr_Shutdown(); return result; }
@@ -71,19 +84,11 @@ int main() {
 	ovrHmdDesc hmdDesc = ovr_GetHmdDesc(session);
 	printHmdInfo(hmdDesc);
 
-	std::cout << "Press Q to quit." << std::endl;
-	int timeStep = 0;
-	while (true) {
-		if (keyboardInterrupt()) break;
-		ovrTrackingState ts = ovr_GetTrackingState(session, ovr_GetTimeInSeconds(), ovrTrue);
-		printPositionAndOrientation(ts, timeStep);
-		timeStep++;
-		Sleep(10);
-	}
-	std::cout << std::endl;
 
-	ovr_Destroy(session);
-	ovr_Shutdown();
-	std::cout << "Bye, Rift!" << std::endl;
+	std::cout << "Press Q to quit." << std::endl;
+	glutDisplayFunc(glutDisplay);
+	glutIdleFunc(glutIdle);
+	glutKeyboardFunc(glutKeyboard);
+	glutMainLoop();
 	return 0;
 }
