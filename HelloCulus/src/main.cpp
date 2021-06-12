d9ae86aed108df8bf02985a02a47ea4098f5b8ff
Uğur Güney <ugurguney@gmail.com>
2021-06-12 00:21:22 -0400

    Non-blocking keyboard input
    To have an infinite loop in which we can display HDM tracking data

--- HelloCulus/src/main.cpp
+++ HelloCulus/src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iomanip>
+#include <Windows.h>
 
 #include <OVR_CAPI.h>
 
@@ -12,8 +14,35 @@ void printHmdInfo(const ovrHmdDesc& desc) {
 	// AvailableHmdCaps, DefaultHmdCaps, AvailableTrackingCaps, DefaultTrackingCaps, DefaultEyeFov, MaxEyeFov
 }
 
+// Non-blocking keyboard input
+DWORD numEvents;
+INPUT_RECORD irInBuf[128]; // buffer to read into
+HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE); // input buffer handle
+bool keyboardInterrupt() {
+	bool shouldExit = false;
+	PeekConsoleInput(hStdin, irInBuf, 128, &numEvents);
+	if (numEvents > 0) {
+		ReadConsoleInput(hStdin, irInBuf, 128, &numEvents);
+		for (int i = 0; i < numEvents; i++) {
+			if (irInBuf[i].EventType != KEY_EVENT)
+				continue;
+			KEY_EVENT_RECORD ev = irInBuf[i].Event.KeyEvent;
+			if (!ev.bKeyDown)
+				continue;
+			switch (ev.uChar.AsciiChar) {
+			case 'q':
+			case 'Q':
+				shouldExit = true;
+			}
+			//std::cout << "key code: " << ev.wVirtualKeyCode << ", ...: " << ev.wVirtualScanCode << ", ...: " << ev.uChar.AsciiChar << std::endl;
+		}
+	}
+	return shouldExit;
+}
+
 int main() {
 	std::cout << "Hello, Rift!" << std::endl;
+
 	ovrResult result = ovr_Initialize(nullptr);
 	if (OVR_FAILURE(result)) { std::cout << "Initialization failed with result code " << result << std::endl;  return result; }
 
@@ -24,6 +53,16 @@ int main() {
 
 	ovrHmdDesc hmdDesc = ovr_GetHmdDesc(session);
 	printHmdInfo(hmdDesc);
+
+	std::cout << "Press Q to quit." << std::endl;
+	int timeStep = 0;
+	while (true) {
+		if (keyboardInterrupt()) break;
+		std::cout << "\rtime: " << std::setw(5) << timeStep << std::flush;
+		timeStep++;
+		Sleep(100);
+	}
+	std::cout << std::endl;
 
 	ovr_Destroy(session);
 	ovr_Shutdown();
