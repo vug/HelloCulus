57410197ab71e6443940b8756186d423d9697bce
Uğur Güney <ugurguney@gmail.com>
2021-06-11 23:46:46 -0400

    Initialize OVR
    Initialize, print HMD info, shutdown.

--- HelloCulus/src/main.cpp
+++ HelloCulus/src/main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+
+#include <OVR_CAPI.h>
+
+void printHmdInfo(const ovrHmdDesc& desc) {
+	std::cout << "Head Mounted Display Info" << std::endl;
+	std::cout << "  Type: " << desc.Type << ". (14 is ovrHmd_CV1)" << std::endl;
+	std::cout << "  Manufacturer: " << desc.Manufacturer << std::endl;
+	std::cout << "  Firmware Version. Major: " << desc.FirmwareMajor << ", Minor: " << desc.FirmwareMinor << std::endl;
+	std::cout << "  Resolution. Width: " << desc.Resolution.w << ", Height: " << desc.Resolution.h << std::endl;
+	std::cout << "  Display Refresh Rate: " << desc.DisplayRefreshRate << std::endl;
+	// AvailableHmdCaps, DefaultHmdCaps, AvailableTrackingCaps, DefaultTrackingCaps, DefaultEyeFov, MaxEyeFov
+}
+
 int main() {
 	std::cout << "Hello, Rift!" << std::endl;
+	ovrResult result = ovr_Initialize(nullptr);
+	if (OVR_FAILURE(result)) { std::cout << "Initialization failed with result code " << result << std::endl;  return result; }
+
+	ovrSession session;
+	ovrGraphicsLuid luid;
+	result = ovr_Create(&session, &luid);
+	if (OVR_FAILURE(result)) { std::cout << "Creation failed with result code " << result << std::endl; ovr_Shutdown(); return result; }
+
+	ovrHmdDesc hmdDesc = ovr_GetHmdDesc(session);
+	printHmdInfo(hmdDesc);
+
+	ovr_Destroy(session);
+	ovr_Shutdown();
+	std::cout << "Bye, Rift!" << std::endl;
 	return 0;
 }
